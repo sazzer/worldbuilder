f3c03dcaae0204a59e1e31ee04032d0e28c2875d
Graham Cox <graham@grahamcox.co.uk>
2016-02-06 09:06:49 +0000

    Threw away everything GraphQL for now. This can and will be added back later

--- src/server/server/index.js
+++ src/server/server/index.js
@@ -1,21 +1,10 @@
 import express from 'express';
-import graphqlHttp from 'express-graphql';
-import {schema} from './graphql';
 import {authenticateRequest} from './authentication/middleware';
 
 export function createServer() {
     const app = express();
 
     app.use(authenticateRequest);
-
-    app.use('/api/graphql', graphqlHttp(request => ({
-        schema: schema,
-        pretty: true,
-        graphiql: true,
-        rootValue: {
-            authentication: request.authentication
-        }
-    })));
 
     return app;
 }
